c3ae646f01b69145cbb33197a18e5c5e02332551
DavidmMar <davidmm@live.com.pt>
2023-04-15 13:33:48 +0100

    changed mqtt library

--- lab/mqtt_client_sub/JS Example/mqtt_sub.js
+++ lab/mqtt_client_sub/JS Example/mqtt_sub.js
@@ -1,32 +1,16 @@
-// Create a client instance
-var client = new Paho.MQTT.Client(location.hostname, Number(location.port), "clientId");
+const mqtt = require('mqtt')
+const client  = mqtt.connect([{ host: 'localhost', port: 1883 }])
 
-// set callback handlers
-client.onConnectionLost = onConnectionLost;
-client.onMessageArrived = onMessageArrived;
+client.on('connect', function () {
+  client.subscribe('presence', function (err) {
+    if (!err) {
+      console.log("no error!\n");
+    }
+  })
+})
 
-// connect the client
-client.connect({onSuccess:onConnect});
-
-
-// called when the client connects
-function onConnect() {
-  // Once a connection has been made, make a subscription and send a message.
-  console.log("onConnect");
-  client.subscribe("World");
-  message = new Paho.MQTT.Message("Hello");
-  message.destinationName = "World";
-  client.send(message);
-}
-
-// called when the client loses its connection
-function onConnectionLost(responseObject) {
-  if (responseObject.errorCode !== 0) {
-    console.log("onConnectionLost:"+responseObject.errorMessage);
-  }
-}
-
-// called when a message arrives
-function onMessageArrived(message) {
-  console.log("onMessageArrived:"+message.payloadString);
-}
+client.on('message', function (topic, message) {
+  // message is Buffer
+  console.log(message.toString())
+  client.end()
+})
