fa6d273d44612740389db3edc9ff6a0bf9071939
DavidmMar <davidmm@live.com.pt>
2024-09-16 15:14:25 +0100

    started java spring implementation

--- server/hms/src/main/java/hms/HmsApplication.java
+++ server/hms/src/main/java/hms/HmsApplication.java
@@ -12,6 +12,8 @@ public class HmsApplication {
 
 	public static void main(String[] args) {
 
+		MqttSubscriber.run();
+
 		SpringApplication.run(HmsApplication.class, args);
 	}
 
---
+++ server/hms/src/main/java/hms/MqttSubscriber.java
@@ -0,0 +1,57 @@
+package hms;
+
+import org.eclipse.paho.client.mqttv3.*;
+
+public class MqttSubscriber {
+
+    public static void run() {
+        String broker = "xxx";
+        String clientId = "hms_server";
+
+        MqttClient client = connect(broker, clientId);
+
+        client.setCallback(new MqttCallback() {
+
+            @Override
+            public void connectionLost(Throwable throwable) {
+                System.out.println("connectionLost: " + throwable.getMessage());
+            }
+
+            @Override
+            public void deliveryComplete(IMqttDeliveryToken iMqttDeliveryToken) {
+                System.out.println("deliveryComplete: " + iMqttDeliveryToken.isComplete());
+            }
+
+            @Override
+            public void messageArrived(String topic, MqttMessage msg) throws Exception {
+                System.out.println("topic: " + topic);
+                System.out.println("qos: " + msg.getQos());
+                System.out.println("message content: " + new String(msg.getPayload()));
+
+                //rdb.storeMessage
+            }
+        });
+
+        String topic = "solar_cell_1";
+        int qos = 0;
+
+        try {
+            client.subscribe(topic, qos);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static MqttClient connect(String broker, String clientId) {
+        try {
+            MqttClient client = new MqttClient(broker, clientId);
+            MqttConnectOptions options = new MqttConnectOptions();
+            client.connect(options);
+
+            return client;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
